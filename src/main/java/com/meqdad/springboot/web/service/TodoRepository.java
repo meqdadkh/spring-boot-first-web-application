b280f457aa239ba2649b708e254777f00f67879a
Meqdad <e_khameyar@isc.co.ir>
2023-01-11 16:42:22 +0330

    added updateTodo

---
+++ src/main/java/com/meqdad/springboot/web/service/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.meqdad.springboot.web.service;
+
+import com.meqdad.springboot.web.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    List<Todo> findByUsername(String username);
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TODO values (10001, 'Learn Spring Boot', false, null, 'Meqdad');
+insert into TODO values(10002,'Learn Angular JS', false, null, 'Meqdad');
+insert into TODO values(10003,'Learn dot net', false, null, 'Meqdad');
